a58884da98dd5ef6987c51b58007e8aa4598de4e
Ranjitha Seenivasan <ranjithaseenivasan04@gmail.com>
2025-07-14 21:18:34 +0530

    🧠 DSA Day 1: Solved Largest & Second Largest (Brute, Better, Optimal)

---
+++ largestElement.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+
+public class largestElement {
+    /*Using sorting 
+    Time = O(n log n) 
+    space = O(1)*/
+    public static int largestElementBrute(int arr[]){
+        Arrays.sort(arr);
+        return arr[arr.length-1];
+    }
+ 
+    /*Using Recursion and max var 
+    Time = O(n) 
+    space = O(1)*/
+    public static int largestElementRecursion(int arr[]){
+       int max = arr[0];
+       for(int i = 0; i < arr.length; i++){
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+       }
+       return max;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2 , 2 , 3 , 0};
+       int large = largestElementRecursion(arr);
+       System.out.println("The largest element in Array is: " + large);
+    }
+}
+ 
---
+++ secondLargest.java
@@ -0,0 +1,107 @@
+
+import java.util.Arrays;
+
+public class secondLargest {
+    /*Time Complexity: O(NlogN), 
+    For sorting the array
+    Space Complexity: O(1) */
+    private static void secondLargestSmallestBrute(int arr[], int n){
+        if(n == 0 || n == 1){
+            System.out.println(-1);
+            System.out.println(" ");
+            System.out.println(-1);
+            System.out.println(" ");
+
+        }
+        Arrays.sort(arr);
+        int small = arr[1];
+        int large = arr[n-2];
+        System.out.println("The second smallest element: " + small);
+        System.out.println("The second largest element: " + large);
+    }
+    
+
+    /*Better solution using traverse the array
+      TC = O(n);
+      SC = O(1)
+     */
+
+
+
+     private static void secondLargestSmallestBetter(int arr[], int n){
+        if(n == 0 || n == 1){
+            System.out.println(-1);
+            System.out.println(" ");
+            System.out.println(-1);
+            System.out.println(" ");
+        }
+        int small = Integer.MAX_VALUE;
+        int large = Integer.MIN_VALUE;
+        int secondSmall = Integer.MAX_VALUE;
+        int secondLarge = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++){
+            small = Math.min(small, arr[i]);
+            large = Math.max(large, arr[i]);
+        }
+        for(int i = 0; i < n; i++){
+            if(arr[i] < secondSmall && arr[i] != small){
+                secondSmall = arr[i];
+            }
+            if(arr[i] > secondLarge && arr[i] != large){
+                secondLarge = arr[i];
+            }
+        }
+        System.out.println("Second Smallest Element is : " + secondSmall);
+        System.out.println("Second Largest Element is : " + secondLarge);
+    }
+
+    //Best Optimal Solutions
+
+    //secondLargest 
+
+    private static int secondLargestOptimal(int arr[], int n){
+        if(n < 2) return -1;
+        int large = Integer.MIN_VALUE;
+        int secondLarge = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] > large){
+                secondLarge = large;
+                large = arr[i];
+            }
+            else if(arr[i] > secondLarge && arr[i] != large){
+                secondLarge = arr[i];
+            }
+        }
+       return (secondLarge == Integer.MIN_VALUE) ? -1 : secondLarge;
+    }
+    //second smallest
+
+    private static int secondSmallestOptimal(int arr[], int n){
+        if(n < 2) return -1;
+        int small = Integer.MAX_VALUE;
+        int secondSmall = Integer.MAX_VALUE;
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] < small){
+                secondSmall = small;
+                small = arr[i];
+            }
+            else if(arr[i] < secondSmall && arr[i] != small){
+                secondSmall = arr[i];
+            }
+        }
+        return secondSmall;
+    }
+
+
+    
+    public static void main(String[] args) {
+        int[] arr = {1,5,8,9,3};
+        int n = arr.length;
+        int result1 = secondLargestOptimal(arr, n);
+        int result2 = secondSmallestOptimal(arr, n);
+        System.out.println("The second largest is: " + result1);
+        System.out.println("The second Smallest is: " + result2);
+    }
+}
